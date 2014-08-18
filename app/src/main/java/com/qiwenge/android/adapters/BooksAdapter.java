27c51d4627c55c81c69dcdea06f550369b5f4fd8
Eric <guangqiang.dev@gmail.com>
2014-08-18 22:16:50 +0800

    Remove AnimateFirstDisplayListener.

--- app/src/main/java/com/qiwenge/android/adapters/BooksAdapter.java
+++ app/src/main/java/com/qiwenge/android/adapters/BooksAdapter.java
@@ -1,11 +1,8 @@
 package com.qiwenge.android.adapters;
 
-import java.util.Collections;
-import java.util.LinkedList;
 import java.util.List;
 
 import android.content.Context;
-import android.graphics.Bitmap;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,35 +10,29 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.nostra13.universalimageloader.core.DisplayImageOptions;
-import com.nostra13.universalimageloader.core.listener.SimpleImageLoadingListener;
 import com.qiwenge.android.R;
 import com.qiwenge.android.adapters.base.MyBaseAdapter;
 import com.qiwenge.android.models.Book;
-import com.qiwenge.android.utils.FadeInDisplayer;
 import com.qiwenge.android.utils.ImageLoaderUtils;
 
 /**
  * BooksAdapter
- * 
+ * <p/>
  * Created by John on 2014-7-6
  */
 public class BooksAdapter extends MyBaseAdapter<Book> {
 
-    private ViewHolder viewHolder;
-
     private DisplayImageOptions mOptions;
-
-    private AnimateFirstDisplayListener animateFirstDisplayListener;
 
     public BooksAdapter(Context context, List<Book> data) {
         this.data = data;
         this.context = context;
         mOptions = ImageLoaderUtils.createOptions(R.drawable.icon_place_holder);
-        animateFirstDisplayListener=new AnimateFirstDisplayListener();
     }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder viewHolder;
         if (convertView == null) {
             viewHolder = new ViewHolder();
             convertView = LayoutInflater.from(context).inflate(R.layout.item_book, null);
@@ -58,27 +49,12 @@ public class BooksAdapter extends MyBaseAdapter<Book> {
             viewHolder.tvTitle.setText(model.title);
             viewHolder.tvDesc.setText(model.description);
             viewHolder.tvAuthor.setText(model.author);
-            ImageLoaderUtils.display(model.cover, viewHolder.ivCover, mOptions,animateFirstDisplayListener);
+            ImageLoaderUtils.display(model.cover, viewHolder.ivCover, mOptions);
         }
         return convertView;
     }
 
-    private static class AnimateFirstDisplayListener extends SimpleImageLoadingListener {
-        static final List<String> images= Collections.synchronizedList(new LinkedList<String>());
-
-        @Override
-        public void onLoadingComplete(String imageUri, View view, Bitmap loadedImage) {
-            if(loadedImage!=null){
-                ImageView imageView=(ImageView)view;
-                if (!images.contains(imageUri)) {
-                    FadeInDisplayer.animate(imageView, 500);
-                    images.add(imageUri);
-                }
-            }
-        }
-    }
-
-    public class ViewHolder {
+    private class ViewHolder {
         public TextView tvTitle;
         public TextView tvAuthor;
         public TextView tvDesc;
