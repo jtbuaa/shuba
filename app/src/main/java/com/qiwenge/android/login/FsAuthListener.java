846c98bfca312d0524a96ab06c1e1bba6cce99d5
Eric <guangqiang.dev@gmail.com>
2014-11-23 15:55:04 +0800

    Added a method onStart()

--- app/src/main/java/com/qiwenge/android/login/FsAuthListener.java
+++ app/src/main/java/com/qiwenge/android/login/FsAuthListener.java
@@ -3,6 +3,8 @@ package com.qiwenge.android.login;
 
 public interface FsAuthListener {
 
+    public void onStart();
+
 	public void authSuccess(String uid, String username,String avatarUrl, LoginType loginType);
 
 }
