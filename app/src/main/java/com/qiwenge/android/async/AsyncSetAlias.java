ac0d0a5b32c5a4997266ad15955ed543a4c0190d
Eric <guangqiang.dev@gmail.com>
2014-11-23 15:24:01 +0800

    Updated AsyncSetAlias.

--- app/src/main/java/com/qiwenge/android/async/AsyncSetAlias.java
+++ app/src/main/java/com/qiwenge/android/async/AsyncSetAlias.java
@@ -4,12 +4,13 @@ import java.util.Set;
 
 import android.content.Context;
 import android.os.AsyncTask;
+
 import cn.jpush.android.api.JPushInterface;
 import cn.jpush.android.api.TagAliasCallback;
 
 /**
  * 初始化极光。
- * 
+ * <p/>
  * Created by John on 2014年7月1日
  */
 public class AsyncSetAlias extends AsyncTask<String, Integer, Boolean> {
@@ -17,19 +18,22 @@ public class AsyncSetAlias extends AsyncTask<String, Integer, Boolean> {
     private Context mContext;
 
     public AsyncSetAlias(Context context) {
-        this.mContext = context;
+        mContext = context;
     }
 
     @Override
     protected Boolean doInBackground(String... params) {
+        System.out.println("设置极光别名");
         if (params != null) {
-            JPushInterface.setDebugMode(true);
-            JPushInterface.init(mContext);
             JPushInterface.setAlias(mContext, params[0], new TagAliasCallback() {
                 @Override
                 public void gotResult(int arg0, String arg1, Set<String> arg2) {
-                    System.out.println("code:" + arg0);
-                    System.out.println("p1:" + arg1);
+                    if (arg0 == 0) {
+                        System.out.println("极光别名设置成功:" + arg1);
+                    } else {
+                        System.out.println("极光别名设置失败:" + arg0);
+                    }
+
                 }
             });
         }
