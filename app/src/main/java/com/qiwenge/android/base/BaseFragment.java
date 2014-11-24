f4b81f1a3ac0fafc551dc8ce2e1868eb09ed4835
Eric <guangqiang.dev@gmail.com>
2014-11-24 13:21:12 +0800

    UPdated.

--- app/src/main/java/com/qiwenge/android/base/BaseFragment.java
+++ app/src/main/java/com/qiwenge/android/base/BaseFragment.java
@@ -2,29 +2,79 @@ package com.qiwenge.android.base;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v4.app.Fragment;
+
+import com.qiwenge.android.login.AuthListener;
+import com.qiwenge.android.login.LoginType;
+import com.qiwenge.android.ui.dialogs.LoginDialog;
 
 /**
  * BaseFragment
- * 
+ * <p/>
  * Created by John on 2014-4-26
  */
 public class BaseFragment extends Fragment {
 
-	@Override
-	public void startActivity(Intent intent) {
-		super.startActivity(intent);
-	}
 
-	public void startActivity(Class<?> cls) {
-		Intent intent = new Intent(getActivity(), cls);
-		startActivity(intent);
-	}
+    private LoginDialog loginDialog;
+    private String mUserName;
+    private String mAvatarUrl;
 
-	public void startActivity(Class<?> cls, Bundle extra) {
-		Intent intent = new Intent(getActivity(), cls);
-		intent.putExtras(extra);
-		startActivity(intent);
-	}
+    public void showAuthDialog() {
+        if (loginDialog == null) {
+            loginDialog = new LoginDialog(getActivity());
+            loginDialog.setAuthListener(new AuthListener() {
+
+                @Override
+                public void onStart() {
+                    onAuthStart();
+                }
+
+                @Override
+                public void authSuccess(String uid, String username, String avatarUrl, LoginType loginType) {
+                    mAvatarUrl = avatarUrl;
+                    mUserName = username;
+                    Message msg = new Message();
+                    msg.what = 1;
+                    mHandler.sendMessage(msg);
+                }
+            });
+        }
+        loginDialog.show();
+    }
+
+    private final Handler mHandler = new Handler(new Handler.Callback() {
+        @Override
+        public boolean handleMessage(Message msg) {
+            if (msg.what == 1) {
+                onAuthSuccess(mUserName, mAvatarUrl);
+            }
+            return false;
+        }
+    });
+
+    public void onAuthStart() {
+    }
+
+    public void onAuthSuccess(String username, String avatarUrl) {
+    }
+
+    @Override
+    public void startActivity(Intent intent) {
+        super.startActivity(intent);
+    }
+
+    public void startActivity(Class<?> cls) {
+        Intent intent = new Intent(getActivity(), cls);
+        startActivity(intent);
+    }
+
+    public void startActivity(Class<?> cls, Bundle extra) {
+        Intent intent = new Intent(getActivity(), cls);
+        intent.putExtras(extra);
+        startActivity(intent);
+    }
 
 }
--- app/src/main/java/com/qiwenge/android/fragments/MeFragment.java
+++ app/src/main/java/com/qiwenge/android/fragments/MeFragment.java
@@ -1,6 +1,5 @@
 package com.qiwenge.android.fragments;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
@@ -17,10 +16,6 @@ import com.qiwenge.android.R;
 import com.qiwenge.android.act.FeedbackActivity;
 import com.qiwenge.android.act.SettingActivity;
 import com.qiwenge.android.base.BaseFragment;
-import com.qiwenge.android.login.AuthListener;
-import com.qiwenge.android.login.LoginType;
-import com.qiwenge.android.login.SinaWeiboLogin;
-import com.qiwenge.android.ui.dialogs.LoginDialog;
 import com.qiwenge.android.utils.ImageLoaderUtils;
 
 public class MeFragment extends BaseFragment implements View.OnClickListener {
@@ -54,7 +49,7 @@ public class MeFragment extends BaseFragment implements View.OnClickListener {
                 startActivity(FeedbackActivity.class);
                 break;
             case R.id.layout_user:
-                showLogin();
+                showAuthDialog();
                 break;
         }
     }
@@ -72,51 +67,23 @@ public class MeFragment extends BaseFragment implements View.OnClickListener {
         layoutUser.setOnClickListener(this);
     }
 
-    private LoginDialog loginDialog;
-    private String mUserName;
-    private String mAvatarUrl;
 
-    private void showLogin() {
-        if (loginDialog == null) {
-            loginDialog = new LoginDialog(getActivity());
-            loginDialog.setAuthListener(new AuthListener() {
-
-                @Override
-                public void onStart() {
-                    tvUserName.setText("正在登陆...");
-                }
-
-                @Override
-                public void authSuccess(String uid, String username, String avatarUrl, LoginType loginType) {
-                    System.out.println("授权成功");
-                    System.out.println("username:" + username);
-                    System.out.println("avatar:" + avatarUrl);
-                    mAvatarUrl = avatarUrl;
-                    mUserName = username;
-
-                    Message msg = new Message();
-                    msg.what = 1;
-                    mHandler.sendMessage(msg);
-                }
-            });
-        }
-        loginDialog.show();
+    @Override
+    public void onAuthStart() {
+        super.onAuthStart();
+        tvUserName.setText("正在登录...");
     }
 
-    private final Handler mHandler = new Handler(new Handler.Callback() {
-        @Override
-        public boolean handleMessage(Message msg) {
+    @Override
+    public void onAuthSuccess(String username, String avatarUrl) {
+        super.onAuthSuccess(username, avatarUrl);
+        System.out.println("登录成功:"+avatarUrl);
+        DisplayImageOptions options = ImageLoaderUtils.createOptions(R.drawable.default_avatar);
+        ImageLoader.getInstance().displayImage(avatarUrl, ivAvatar, options);
+        tvUserName.setText(username);
+    }
 
-            if (msg.what == 1) {
-                DisplayImageOptions options = ImageLoaderUtils.createOptions(R.drawable.default_avatar);
-                ImageLoader.getInstance().displayImage(mAvatarUrl+"?time=2014-11-23", ivAvatar, options);
-                tvUserName.setText(mUserName);
-            }
-            return false;
-        }
-    });
-
-    private void login(){
+    private void login() {
 
     }
 
