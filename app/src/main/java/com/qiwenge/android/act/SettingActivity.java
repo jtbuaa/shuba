af51e67f03641f6e01c9c8c3bbe166d197b0ef59
Eric <guangqiang.dev@gmail.com>
2015-04-23 23:33:54 +0800

    Added a dependenciy butterknife for injecting.

--- app/src/main/java/com/qiwenge/android/act/SettingActivity.java
+++ app/src/main/java/com/qiwenge/android/act/SettingActivity.java
@@ -2,7 +2,6 @@ package com.qiwenge.android.act;
 
 import android.os.Bundle;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -17,139 +16,60 @@ import com.qiwenge.android.listeners.OnPositiveClickListener;
 import com.qiwenge.android.ui.dialogs.MyDialog;
 import com.qiwenge.android.utils.ImageLoaderUtils;
 import com.qiwenge.android.utils.LoginManager;
-import com.qiwenge.android.utils.PushUtils;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+import butterknife.OnClick;
 
 /**
  * 设置。
  * <p/>
  * Created by Eric on 2014-7-9
  */
-public class SettingActivity extends BaseActivity implements OnClickListener {
+public class SettingActivity extends BaseActivity {
 
-    private TextView tvVersionName;
-    private TextView tvRating;
-    private TextView tvLegal;
-    private TextView tvCheckUpdate;
-    private TextView tvLogout;
-    private LinearLayout layoutUser;
+    @InjectView(R.id.tv_version_name)
+    TextView tvVersionName;
 
-    private ImageView ivPushMessage;
-    private ImageView ivSaveModel;
+    @InjectView(R.id.layout_set_user)
+    LinearLayout layoutUser;
+
+    @InjectView(R.id.iv_save_model)
+    ImageView ivSaveModel;
+
+    private MyDialog logoutDialog;
 
     private boolean selectSaveModel = false;
-
-    private PushUtils pushUtils;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
         setTitle(R.string.set_title);
+        ButterKnife.inject(this);
+
         initViews();
-    }
-
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.iv_push_message:
-                switchPussMessage();
-                break;
-            case R.id.iv_save_model:// 控制流量
-                switchSaveModel();
-                break;
-            case R.id.set_tv_rating:// 评分
-                skipToMarket();
-                break;
-            case R.id.set_tv_legal://免责声明
-                startActivity(LegalActivity.class);
-                break;
-            case R.id.set_tv_update://检查版本更新
-                new AsyncCheckUpdate(this).checkUpdate();
-                break;
-            case R.id.set_tv_logout:
-                showLogoutDialog();
-                break;
-            default:
-                break;
-        }
-    }
-
-    private void skipToMarket() {
-        try {
-            IntentUtils.skipToMarket(SettingActivity.this);
-        } catch (Exception ex) {
-            ToastUtils.show(getApplicationContext(), getString(R.string.error_not_find_market));
-        }
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        if (LoginManager.isLogin()) {
-            layoutUser.setVisibility(View.VISIBLE);
-        } else {
-            layoutUser.setVisibility(View.GONE);
-        }
+        layoutUser.setVisibility(LoginManager.isLogin() ? View.VISIBLE : View.GONE);
     }
 
-
     private void initViews() {
-        layoutUser = (LinearLayout) this.findViewById(R.id.layout_set_user);
-
-        tvLegal = (TextView) this.findViewById(R.id.set_tv_legal);
-        tvLegal.setOnClickListener(this);
-
-        tvRating = (TextView) this.findViewById(R.id.set_tv_rating);
-        tvRating.setOnClickListener(this);
-
-        tvVersionName = (TextView) this.findViewById(R.id.tv_version_name);
         tvVersionName.setText(AppUtils.getVersionName(getApplicationContext()));
-
-        ivPushMessage = (ImageView) this.findViewById(R.id.iv_push_message);
-        ivPushMessage.setOnClickListener(this);
-        ivPushMessage.setTag(1);
-
-        ivSaveModel = (ImageView) this.findViewById(R.id.iv_save_model);
-        ivSaveModel.setOnClickListener(this);
-        ivSaveModel.setTag(1);
-
-        tvCheckUpdate = (TextView) this.findViewById(R.id.set_tv_update);
-        tvCheckUpdate.setOnClickListener(this);
-
-        tvLogout = (TextView) this.findViewById(R.id.set_tv_logout);
-        tvLogout.setOnClickListener(this);
-
         if (!ImageLoaderUtils.isOpen()) {
             setCheckbox(ivSaveModel, true);
             selectSaveModel = true;
-        }
-
-        pushUtils = new PushUtils(this);
-
-        if (pushUtils.isOpenPush()) {
-            setCheckbox(ivPushMessage, true);
-        } else {
-            setCheckbox(ivPushMessage, false);
-        }
-    }
-
-    /**
-     * 切换推送消息开关
-     */
-    private void switchPussMessage() {
-        if (pushUtils.isOpenPush()) {
-            pushUtils.stopPush();
-            setCheckbox(ivPushMessage, false);
-        } else {
-            pushUtils.openPush();
-            setCheckbox(ivPushMessage, true);
         }
     }
 
     /**
      * 切换流量模式
      */
-    private void switchSaveModel() {
+    @OnClick(R.id.iv_save_model)
+    public void switchSaveModel() {
         if (selectSaveModel) {
             setCheckbox(ivSaveModel, false);
             ImageLoaderUtils.openLoader(getApplicationContext());
@@ -160,17 +80,27 @@ public class SettingActivity extends BaseActivity implements OnClickListener {
         selectSaveModel = !selectSaveModel;
     }
 
-    private void setCheckbox(ImageView iv, boolean ischecked) {
-        if (ischecked) {
-            iv.setBackgroundResource(R.drawable.icon_switch_on);
-        } else {
-            iv.setBackgroundResource(R.drawable.icon_switch_off);
+    @OnClick(R.id.set_tv_rating)
+    public void skipToMarket() {
+        try {
+            IntentUtils.skipToMarket(SettingActivity.this);
+        } catch (Exception ex) {
+            ToastUtils.show(getApplicationContext(), getString(R.string.error_not_find_market));
         }
     }
 
-    private MyDialog logoutDialog;
+    @OnClick(R.id.set_tv_legal)
+    public void skipToLegal() {
+        startActivity(LegalActivity.class);
+    }
 
-    private void showLogoutDialog() {
+    @OnClick(R.id.set_tv_update)
+    public void chkVersionUpdate() {
+        new AsyncCheckUpdate(this).checkUpdate();
+    }
+
+    @OnClick(R.id.set_tv_logout)
+    public void showLogoutDialog() {
         logoutDialog = new MyDialog(this, R.string.set_logout_title);
         logoutDialog.setMessage(R.string.set_logout_msg);
         logoutDialog.setPositiveButton(R.string.str_sure, new OnPositiveClickListener() {
@@ -181,6 +111,14 @@ public class SettingActivity extends BaseActivity implements OnClickListener {
             }
         });
         logoutDialog.show();
+    }
+
+    private void setCheckbox(ImageView iv, boolean ischecked) {
+        if (ischecked) {
+            iv.setBackgroundResource(R.drawable.icon_switch_on);
+        } else {
+            iv.setBackgroundResource(R.drawable.icon_switch_off);
+        }
     }
 
 }
